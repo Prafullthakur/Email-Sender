36647153a11893754bc51193feaff683435eb081
Prafullthakur <prafullchauhan49.gmail.com>
2020-06-14 00:21:42 +0530

    add

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
-const esphbs = require('express-handlebars');
+const exphbs = require('express-handlebars');
+const path = require('path');
 
+// Init app
 const app = express();
+
+// Templete engine
+app.engine('handlebars', exphbs);
+app.set('view engine', 'handlebars');
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json);
+
+app.use(express.static(__dirname + 'public'));
 
 app.get('/', (req, res) => {
     res.send('hello');
 });
 
-const port = 3000;
+const Port = 8000;
 
-app.listen(port, () => {
-    console.log(`Server started on Port: ${port}`);
+app.listen(Port, () => {
+    console.log(`Server started on Port: ${Port}`);
 });
